191c16792d58921b36236aa3fbae3748dd04c1ea
Wyatt Jackson <minecraftchest1@outlook.com>
2021-02-25 20:30:05 +0000

    Move php files to www. Creae 404-1.html

--- www/403.php
+++ www/403.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>403 - Forbidden</h1>
-    <p>You have attempted to access an resoursce (<b><i><?php echo $_SERVER['REQUEST_URI']; ?></i></b>) that you do not have access to.</p>
+    <p>The resoursce (<b><i><?php echo $_SERVER['REQUEST_URI']; ?></i></b>) is restricted, and you do not appear to have access.</p>
     <hr />
     <footer>
         <address>
